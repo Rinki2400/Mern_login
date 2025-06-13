18adcde34b0d23465b4386b3d40c90766aba6501
RinkiShara <rinki2001sharma@gmail.com>
2025-06-14 00:54:57 +0530

    controller add

--- server/controller/appcontroller.js
+++ server/controller/appcontroller.js
@@ -1,10 +1,58 @@
-const express = require('express');
-const router = express.Router();
-
-// Sample controller function
- const getHomePage = (req, res) => {
-    res.status(200).send("Welcome to the Home Page!");
+// for registered  api http://localhost:8000/api/registered
+const registered = (req, res) => {
+  res.status(200).send("Welcome to the Home Page!");
 };
 
+// for login api http://localhost:8080/api/login
 
-module.exports = router;
+const login = (req, res) => {
+  res.status(200).send("login page");
+};
+
+// for login api http://localhost:8080/api/registratemail
+const registratemail = (req, res) => {
+  res.status(200).send("registratemail page");
+};
+
+// for login api http://localhost:8080/api/authenticate
+const authenticate = (req, res) => {
+  res.status(200).send("authenticate page");
+};
+
+// for login api http://localhost:8080/api/user/exampl121
+const user = (req, res) => {
+  res.status(200).send("user page");
+};
+// for login api http://localhost:8080/api/generateOTP
+const generateOTP = (req, res) => {
+  res.status(200).send("generateOTP page");
+};
+// for login api http://localhost:8080/api/vertifyOTP
+const vertifyOTP = (req, res) => {
+  res.status(200).send("vertifyOTP page");
+};
+
+// for login api http://localhost:8080/api/createreset
+const createreset = (req, res) => {
+  res.status(200).send("createreset page");
+};
+// for login api http://localhost:8080/api/updateUser
+const updateUser = (req, res) => {
+  res.status(200).send("updateUser page");
+};
+// for login api http://localhost:8080/api/resetPassword
+const resetPassword = (req, res) => {
+  res.status(200).send("resetPassword page");
+};
+module.exports = {
+  registered,
+  login,
+  registratemail,
+  authenticate,
+  user,
+  generateOTP,
+  vertifyOTP,
+  createreset,
+  updateUser,
+  resetPassword,
+};
--- server/router/routes.js
+++ server/router/routes.js
@@ -1,48 +1,40 @@
-const express = require('express');
-const getHomePage = require('../controller/appcontroller');
-const router = express.Router();
+const express = require("express");
+const {
+  registered,
+  login,
+  registratemail,
+  authenticate,
+  user,
+  generateOTP,
+  vertifyOTP,
+  createreset,
+  updateUser,
+  resetPassword,
 
+} = require("../controller/appcontroller");
+const router = express.Router();
 
 // put routes
 
-
 // post routes
-router.post('/registered', getHomePage);
+router.post("/registered", registered);
 
-router.post('/registratemail',(req, res) =>{
-    res.send("registred email")
-})
+router.post("/registratemail", registratemail);
 
-router.post('/login',(req,res) =>{
-    res.send("login")
-})
+router.post("/login", login);
 
-router.post("/authenticate",(req,res)=>{
-    res.send("authenticate")
-})
+router.post("/authenticate", authenticate);
 
 // get routes
 
-router.get('/user/:username', (req, res) => {
-    res.send('Login page');
-});
-router.get('/generateOTP',(req,res) => {
-    res.send('generate OTP')
-})
-router.get('/vertifyOTP',(req,res) =>{
-    res.send('verified otp')
-})
+router.get("/user/:username", user);
+router.get("/generateOTP", generateOTP);
+router.get("/vertifyOTP", vertifyOTP);
 
-router.get("/createreset",(req,res) =>{
-    res.send("create session ")
-})
+router.get("/createreset", createreset);
 
 // put router
- router.put("/updateUser",(req,res) =>{
-    res.send("Update User")
- })
+router.put("/updateUser", updateUser);
 
- router.put("/resetPassword",(req,res) =>{
-    res.send("Reset Password")
- })
+router.put("/resetPassword",resetPassword);
 module.exports = router;
