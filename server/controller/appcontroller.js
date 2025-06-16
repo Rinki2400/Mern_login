313953ef4de0e4332e22215035105c7342859835
RinkiShara <rinki2001sharma@gmail.com>
2025-06-16 16:47:46 +0530

    update body by id param

--- server/controller/appcontroller.js
+++ server/controller/appcontroller.js
@@ -139,7 +139,7 @@ const updateUser = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.json(updatedUser);
+    res.send({msg:"Record Updated"});
   } catch (error) {
     res.status(500).json({ message: 'Server error', error });
   }
